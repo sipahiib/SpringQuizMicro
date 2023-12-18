09c666f531fdf290036c7382a20cc8023415faf4
İbrahim <73345010+sipahiib@users.noreply.github.com>
2023-12-18 17:13:15 +0300

    exchangeservice

--- exchange_service/src/main/java/com/microservices/exchangeservice/CurrencyExchangeRepository.java
+++ exchange_service/src/main/java/com/microservices/exchangeservice/CurrencyExchangeRepository.java
@@ -1,5 +1,4 @@
 package com.microservices.exchangeservice;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
---
+++ exchange_service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10001,'USD','INR',65,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10002,'EUR','INR',75,'');
