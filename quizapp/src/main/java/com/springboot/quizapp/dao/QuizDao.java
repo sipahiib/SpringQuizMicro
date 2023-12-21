45c4c61a9d6b7d6e93a3ac93db7fb3a60fb3418b
İbrahim <73345010+sipahiib@users.noreply.github.com>
2023-12-21 15:04:35 +0300

    Delete quizapp directory

--- quizapp/src/main/java/com/springboot/quizapp/dao/QuizDao.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.quizapp.dao;
-
-import com.springboot.quizapp.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizDao extends JpaRepository<Quiz, Integer> {
-}
